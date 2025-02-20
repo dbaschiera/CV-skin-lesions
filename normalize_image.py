426ec49715412c0bfdca75d128c7e451a96b58f7
LucaBarda <53540072+LucaBarda@users.noreply.github.com>
2025-02-20 15:01:40 +0100

    pushami tutto

--- normalize_image.py
+++ normalize_image.py
@@ -12,12 +12,15 @@ def remove_hair(image):
 
     # Apply morphological closing (X · Y)
     closed = cv2.morphologyEx(gray, cv2.MORPH_CLOSE, kernel)
+    # Closing is dilation followed by erosion
+    # It helps to close small holes in the foreground objects (black hair pixels)
 
     # Compute Black Top-Hat transformation: B_TH = (X · Y) - X
     black_tophat = closed - gray
 
     # Threshold to create a binary mask of hair pixels
     _, mask = cv2.threshold(black_tophat, 10, 255, cv2.THRESH_BINARY)
+    # Pixels that are white in closed but black in gray are likely hair pixels
 
     # Inpaint the detected hair pixels using neighboring pixels
     inpainted = cv2.inpaint(image, mask, inpaintRadius=10, flags=cv2.INPAINT_TELEA)
@@ -37,7 +40,8 @@ def resize_image(image, target_size=256, interpolation=cv2.INTER_LINEAR):
 target_size = 256
 
 # Target image
-image_path = 'my_skin_lesions/left_arm.jpg'
+image_path = 'my_skin_lesions/chest1.jpg'
+write_path = 'my_normalized_skin_lesions/chest1.jpg'
 
 # Read image
 image = cv2.imread(image_path)
@@ -48,5 +52,5 @@ hair_removed_image = remove_hair(image)
 # Step 2: Resize the hair-free image and mask to 256×256
 resized_hair_removed = resize_image(hair_removed_image, target_size, interpolation=cv2.INTER_LINEAR)
 
-cv2.imwrite('my_normalized_skin_lesions/left_arm.jpg', resized_hair_removed)  # 256x256 resized
+cv2.imwrite(write_path, resized_hair_removed)  # 256x256 resized
 
